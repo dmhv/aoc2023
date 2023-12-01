838ee28c997ed7ebbf732e090905ff0ffd0cbbdb
dmh <dmelichov@gmail.com>
2023-12-01 22:12:35 +0100

    Reformat

--- src/01p2.kt
+++ src/01p2.kt
@@ -5,15 +5,8 @@ fun main() {
 
 private fun extractDigits(it: String): List<String> {
     val t2d = mapOf(
-        "one" to "1",
-        "two" to "2",
-        "three" to "3",
-        "four" to "4",
-        "five" to "5",
-        "six" to "6",
-        "seven" to "7",
-        "eight" to "8",
-        "nine" to "9",
+        "one" to "1", "two" to "2", "three" to "3", "four" to "4",
+        "five" to "5", "six" to "6", "seven" to "7", "eight" to "8", "nine" to "9",
     )
     val idxToDigit = mutableMapOf<Int, String>()
 
