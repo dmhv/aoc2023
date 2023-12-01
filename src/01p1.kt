85d19c5ac9d4d247290eed5271cdb31c72cf601a
dmh <dmelichov@gmail.com>
2023-12-01 10:32:19 +0100

    Day1

--- src/01p1.kt
+++ src/01p1.kt
@@ -1,19 +1,14 @@
-/**
- * This is a placeholder solution for day 1 of 2018
- */
-import org.jetbrains.kotlinx.multik.api.mk
-import org.jetbrains.kotlinx.multik.api.ndarray
-import org.jetbrains.kotlinx.multik.ndarray.operations.sum
-
 fun main() {
     val lines = readInput("01")
-    val a = mk.ndarray(mk[1, 2, 3])
-    a.sum().println()
-    lines.map { lineToInt(it) } .reduce { acc, i -> acc + i } .println()
+    var out = 0
+    lines.forEach {
+        val ints = extractInts(it)
+        val firstAndLast = "${ints.first()}${ints.last()}"
+        out += firstAndLast.toInt()
+    }
+    println(out)
 }
 
-private fun lineToInt(it: String): Int {
-    val sign = it.take(0)
-    val res = it.drop(0).toInt()
-    return if (sign == "-") res * -1 else res
+private fun extractInts(it: String): List<Char> {
+    return it.filter { it.isDigit() }.toList()
 }
--- src/01p2.kt
+++ src/01p2.kt
@@ -1,28 +1,49 @@
-import kotlin.system.exitProcess
-
-/**
- * This is a placeholder solution for day 1 of 2018
- */
-
 fun main() {
     val lines = readInput("01")
-    var cumSum = 0
-    val seen = mutableSetOf<Int>()
-
-    while (true) {
-        for (l in lines) {
-            cumSum += lineToInt(l)
-            if (seen.contains(cumSum)) {
-                cumSum.println()
-                exitProcess(0)
-            }
-            seen.add(cumSum)
-        }
+    var out = 0
+    lines.forEach {
+        val ints = extractInts(it)
+        val firstAndLast = "${ints.first()}${ints.last()}"
+        out += firstAndLast.toInt()
     }
+    println(out)
 }
 
-private fun lineToInt(it: String): Int {
-    val sign = it.take(0)
-    val res = it.drop(0).toInt()
-    return if (sign == "-") res * -1 else res
+private fun extractInts(it: String): List<String> {
+    val t2d = mapOf(
+        "one" to "1",
+        "two" to "2",
+        "three" to "3",
+        "four" to "4",
+        "five" to "5",
+        "six" to "6",
+        "seven" to "7",
+        "eight" to "8",
+        "nine" to "9",
+    )
+    val idxToDigit = mutableMapOf<Int, String>()
+
+    // process proper digits
+    for (target in t2d.values) {
+        val indices = findAllIndices(it, target)
+        indices.forEach { idxToDigit[it] = target }
+    }
+
+    // process digits as text
+    for (key in t2d.keys) {
+        val indices = findAllIndices(it, key)
+        indices.forEach { idxToDigit[it] = t2d[key]!! }
+    }
+
+    return idxToDigit.toSortedMap().values.toList()
+}
+
+private fun findAllIndices(inp: String, target: String): List<Int> {
+    val out = mutableListOf<Int>()
+    var idx = inp.indexOf(target)
+    while (idx >= 0) {
+        out.add(idx)
+        idx = inp.indexOf(target, idx+1)
+    }
+    return out.toList()
 }
