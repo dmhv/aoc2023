b58ef85bffaab99434078b85d10c3b4c950607db
dmh <dmelichov@gmail.com>
2023-12-02 12:31:40 +0100

    02

---
+++ src/02p1.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val lines = readInput("02")
+    var out = 0
+    lines.forEachIndexed { i, s ->
+        val chunks = s.substringAfter(": ").split(";")
+        val anyImpossible = chunks.map { isPossible(it) }.filter { !it }
+        if (anyImpossible.isEmpty()) out += i+1
+    }
+    out.println()
+}
+
+private fun isPossible(inp: String): Boolean {
+    val counts = mapOf("red" to 12, "green" to 13, "blue" to 14)
+    val pairs = inp.trim().split(", ")
+    pairs.forEach {
+        val numAndColor = it.split(" ")
+        val num = numAndColor[0].toInt()
+        val color = numAndColor[1]
+        if (counts[color]!! < num) return false
+    }
+    return true
+}
---
+++ src/02p2.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val lines = readInput("02")
+    lines.sumOf { minSetPower(it) }.println()
+}
+
+private fun minSetPower(inp: String): Int {
+    val chunks = inp.substringAfter(":").split(";")
+    val counts = mutableMapOf("red" to 0, "green" to 0, "blue" to 0)
+    chunks.forEach { chunk ->
+        val pairs = chunk.trim().split(", ")
+        pairs.forEach {
+            val numAndColor = it.split(" ")
+            val num = numAndColor[0].toInt()
+            val color = numAndColor[1]
+            if (counts[color]!! < num) counts[color] = num
+        }
+    }
+    return counts.values.fold(1) { total, it -> total * it }
+}
