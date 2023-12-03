07820ef91bb561cea0b3952d1fffa42178352482
dmh <dmelichov@gmail.com>
2023-12-03 10:35:42 +0100

    03

---
+++ src/03p1.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val lines = readInput("03")
+
+    data class Num(val row: Int, val colStart: Int, val colEnd: Int, val value: Int)
+    data class Anchor(val row: Int, val col: Int)
+
+    val regexNum = Regex("(\\d+)")
+    val regexAnchor = Regex("([^0-9.])")
+
+    fun Num.isAdjacentTo(anchor: Anchor) =
+        (anchor.row in (this.row-1)..(this.row+1)) && (anchor.col in (this.colStart-1)..(this.colEnd+1))
+
+    fun Num.isAdjacentToAnyOf(anchors: List<Anchor>) = anchors.any { this.isAdjacentTo(it) }
+
+    val numbers = mutableListOf<Num>()
+    val anchors = mutableListOf<Anchor>()
+    for ((r, line) in lines.withIndex()) {
+        val numbersFound = regexNum.findAll(line)
+        numbersFound.forEach { numbers.add(Num(r, it.range.first, it.range.last, it.value.toInt())) }
+
+        val anchorsFound = regexAnchor.findAll(line)
+        anchorsFound.forEach { anchors.add(Anchor(r, it.range.first)) }
+    }
+
+    numbers.filter { it.isAdjacentToAnyOf(anchors) }.sumOf { it.value }.println()
+}
+
---
+++ src/03p2.kt
@@ -0,0 +1,32 @@
+fun main() {
+    val lines = readInput("03")
+
+    data class Num(val row: Int, val colStart: Int, val colEnd: Int, val value: Int)
+    data class Anchor(val row: Int, val col: Int, val value: String)
+
+    val regexNum = Regex("(\\d+)")
+    val regexAnchor = Regex("([^0-9.])")
+
+    fun Num.isAdjacentTo(anchor: Anchor) =
+        (anchor.row in (this.row-1)..(this.row+1)) && (anchor.col in (this.colStart-1)..(this.colEnd+1))
+
+    fun Anchor.countAdjacent(ns: List<Num>) = ns.filter { it.isAdjacentTo(this) }.size
+
+    val numbers = mutableListOf<Num>()
+    val anchors = mutableListOf<Anchor>()
+    for ((r, line) in lines.withIndex()) {
+        val numbersFound = regexNum.findAll(line)
+        numbersFound.forEach { numbers.add(Num(r, it.range.first, it.range.last, it.value.toInt())) }
+
+        val anchorsFound = regexAnchor.findAll(line)
+        anchorsFound.forEach { anchors.add(Anchor(r, it.range.first, it.value)) }
+    }
+
+    anchors
+        .filter { it.value == "*" }
+        .filter { it.countAdjacent(numbers) == 2 }
+        .map {gear -> numbers.filter { it.isAdjacentTo(gear) }.fold(1) { acc, it -> acc * it.value }}
+        .sum()
+        .println()
+}
+
