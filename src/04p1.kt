8acb634d7eff1ca141fe783a4273ba3edbca5b35
dmh <dmelichov@gmail.com>
2023-12-04 08:43:25 +0100

    04

---
+++ src/04p1.kt
@@ -0,0 +1,24 @@
+import kotlin.math.pow
+
+fun main() {
+    val lines = readInput("04")
+    val regexNum = Regex("\\s+")
+
+    data class Card(val present: Set<Int>, val winning: Set<Int>)
+
+    fun Card.score(): Int {
+        val numMatched = present.intersect(winning).size
+        if (numMatched == 0) return 0
+        return 2.toDouble().pow(numMatched - 1).toInt()
+    }
+
+    val cards = mutableListOf<Card>()
+    for (line in lines) {
+        val (winningStr, presentStr) = line.split(": ")[1].trim().split("|")
+        val winningNums = regexNum.split(winningStr.trim()).map { it.toInt() }.toSet()
+        val presentNums = regexNum.split(presentStr.trim()).map { it.toInt() }.toSet()
+        cards.add(Card(presentNums, winningNums))
+    }
+    cards.sumOf { it.score() }.println()
+}
+
---
+++ src/04p2.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val lines = readInput("04")
+    val regexNum = Regex("\\s+")
+
+    data class Card(val present: Set<Int>, val winning: Set<Int>)
+
+    fun Card.numMatches() = present.intersect(winning).size
+
+    val numCopies = mutableMapOf<Int, Int>()
+    for (line in lines) {
+        val cardId = line.removePrefix("Card").trim().split(":")[0].toInt()
+        val (winningStr, presentStr) = line.split(": ")[1].trim().split("|")
+        val winningNums = regexNum.split(winningStr.trim()).map { it.toInt() }.toSet()
+        val presentNums = regexNum.split(presentStr.trim()).map { it.toInt() }.toSet()
+
+        val cntThisCard = numCopies.getOrDefault(cardId, 0) + 1
+        numCopies[cardId] = cntThisCard
+
+        for (i in 1..Card(presentNums, winningNums).numMatches()) {
+            val otherCardId = cardId + i
+            val cntOtherCard = numCopies.getOrDefault(otherCardId, 0) + cntThisCard
+            numCopies[otherCardId] = cntOtherCard
+        }
+    }
+    numCopies.values.sum().println()
+}
+
