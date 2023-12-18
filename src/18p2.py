d376a060e1fa0dcaca47cee8c1c9a44c9cf98f11
dmh <dmelichov@gmail.com>
2023-12-18 17:55:20 +0100

    Rename and add more detailed prints

--- src/18p2.py
+++ src/18p2.py
@@ -1,10 +1,10 @@
-with open("../inp/18.txt", 'r') as f:
+with open("../inp/18tiny.txt", 'r') as f:
     lines = f.readlines()
 
 r = 0
 c = 0
 points = [(r, c)]
-boundary_points = 0
+cnt_boundary_points = 0
 for line in lines:
     hex_value = line.split(" ")[2].strip("()#\n")
     d = hex_value[-1]
@@ -18,12 +18,15 @@ for line in lines:
     else:
         r -= int(cnt)
     points.append((r, c))
-    boundary_points += int(cnt)
+    cnt_boundary_points += int(cnt)
 
 area = 0
 for (p1, p0) in zip(points[1:], points[:-1]):
     area += (p0[0] + p1[0]) * (p0[1] - p1[1])
 area = abs(area) // 2
 
-interior_points = area - boundary_points // 2 + 1
-print(interior_points + boundary_points)
+print("Area (Shoelace formula) is", area)
+print("Count of boundary points is", cnt_boundary_points)
+cnt_interior_points = area - cnt_boundary_points // 2 + 1
+print("Count of interior points (Pick's theorem) is", cnt_interior_points)
+print("Interior + boundary is", cnt_interior_points + cnt_boundary_points)
